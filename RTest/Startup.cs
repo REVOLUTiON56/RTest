6244130ed9181cf01920f054d4f5ec97ce9d80a4
Vladislav Kareev <vladislav.kareev@akvelon.com>
2021-02-28 00:41:04 +0300

    Added WebAPI project template. Configured swagger.

---
+++ RTest/Startup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace RTest
+{
+    public class Startup
+    {
+        private const string API_URL = "api";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddResponseCompression(options => options.EnableForHttps = true);
+            services.Configure<GzipCompressionProviderOptions>(options =>
+            {
+                options.Level = CompressionLevel.Fastest;
+            });
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                c.IncludeXmlComments(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "RTest.xml"));
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseResponseCompression();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger(options => options.RouteTemplate = $"{API_URL}/{{documentName}}/swagger.json");
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = $"{API_URL}";
+                c.SwaggerEndpoint("v1/swagger.json", "RTest v1");
+                c.DocumentTitle = "RTest";
+            });
+
+        }
+    }
+}
